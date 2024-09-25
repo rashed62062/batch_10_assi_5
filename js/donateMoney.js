855fa4ed21efda0d5ae03ca2c8efdb950b92d135
Your Name <your@example.com>
2024-09-25 23:36:10 +0600

    one

--- js/donateMoney.js
+++ js/donateMoney.js
@@ -4,58 +4,45 @@ document
     event.preventDefault();
     const addMoney = getInputFieldValueByID("input-add-money");
 
-    
-
-    //        const pinNumber = getInputFieldValueById("input-pin-number");
-
-    
- 
     if (isNaN(addMoney)) {
       alert("filed to add money");
       return;
     }
+
+    if (addMoney < 0 && addMoney && addMoney <= newBalance) {
+      alert(" failed amount");
+      // document.getElementById('my_modal_6').close();
+      return;
+    }
+
     // wrong way to verify do not try at your  serious website
 
     const balance = getTexFiledValueById("account-balance");
-     const savingBalance = getTexFiledValueById("trip-balance");
-       const   casH =  savingBalance - addMoney;
-       document.getElementById("trip-balance").innerText = casH;
+    const savingBalance = getTexFiledValueById("trip-balance");
+    const casH = savingBalance - addMoney;
+    document.getElementById("trip-balance").innerText = casH;
 
-    const  newBalance = balance + addMoney;
+    const newBalance = balance + addMoney;
 
     document.getElementById("account-balance").innerText = newBalance;
 
     //  add to transaction history
 
-//     const p = document.createElement("p");
-//     p.innerText = ` ${addMoney} Taka is Donated for famine-2024 at Feni, Bangladesh `;
+    const div = document.createElement("div");
+    div.classList.add("bg-stone-300");
 
-//     console.log(p);
-
-//     //  should be a
-//     document.getElementById("history-container").appendChild(p);
-
-
-
-
-//  
-  const div = document.createElement('div');
-  div.classList.add('bg-black')
-
-
-
-div.innerHTML = `
-
+    div.innerHTML = `
 
 <h4 class="text-2xl font-bold"></h4>
 
 <p>${addMoney}Taka is Donated for famine-2024 at Feni, Bangladesh</p>
-<p> Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh Standard Time)</p>
+<p class="text-sm"> Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh Standard Time)</p>
 
-`
- document.getElementById('history-container').appendChild(div);
- });
+`;
+    document.getElementById("history-container").appendChild(div);
+  });
 
+//   function
 
 document.getElementById("show-btn").addEventListener("click", function () {
   showSectionById("add-donate-section");
@@ -67,6 +54,107 @@ document
     showSectionById("transaction-section");
   });
 
+//      card 2
 
-//    card 
+document
+  .getElementById("relief-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoney = getInputFieldValueByID("relief-input");
 
+    // validated
+
+    if (isNaN(addMoney)) {
+      alert("filed to add money");
+      return;
+    }
+
+    if (addMoney < 0 && addMoney && addMoney <= newBalance) {
+      alert(" failed amount");
+      // document.getElementById('my_modal_6').close();
+      return;
+    }
+
+    // wrong way to verify do not try at your  serious website
+
+    const balance = getTexFiledValueById("relief-amount");
+    const savingBalance = getTexFiledValueById("trip-balance");
+    const casH = savingBalance - addMoney;
+    document.getElementById("trip-balance").innerText = casH;
+
+    const newBalance = balance + addMoney;
+
+    document.getElementById("relief-amount").innerText = newBalance;
+
+    //  add to transaction history
+
+    const div = document.createElement("div");
+    div.classList.add("bg-stone-300");
+
+    div.innerHTML = `
+  
+  <h4 class="text-2xl font-bold"></h4>
+  
+  <p>${addMoney}Taka is Donate for Flood Relief in Feni,Bangladesh</p>
+  <p class="text-sm"> Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh Standard Time)</p>
+  
+  `;
+    document.getElementById("history-container").appendChild(div);
+  });
+
+//   function
+
+// card 3
+
+document
+  .getElementById("quota-add-money-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoney = getInputFieldValueByID("quota-input");
+
+
+
+
+
+    
+ if (isNaN(addMoney)){
+  alert('filed to add money');
+  return;
+ }
+
+    
+    if(addMoney < 0 &&  (addMoney) &&  addMoney <= newBalance){
+    
+      alert(' failed amount');
+      // document.getElementById('my_modal_6').close();
+      return;
+    }
+
+    // wrong way to verify do not try at your  serious website
+
+    const balance = getTexFiledValueById("quota-ammount");
+    const savingBalance = getTexFiledValueById("trip-balance");
+    const casH = savingBalance - addMoney;
+    document.getElementById("trip-balance").innerText = casH;
+
+    const newBalance = balance + addMoney;
+
+    document.getElementById("quota-ammount").innerText = newBalance;
+
+    //  add to transaction history
+
+    const div = document.createElement("div");
+    div.classList.add("bg-stone-300");
+
+    div.innerHTML = `
+  
+  <h4 class="text-2xl font-bold"></h4>
+  
+  <p>${addMoney}Taka is Aid for Injured in the Quota Movement</p>
+  <p class="text-sm"> Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh Standard Time)</p>
+  
+  `;
+    document.getElementById("history-container").appendChild(div);
+  });
+
+//   function
